d6eb03aacafd78872eaffdc2aa27984cd531b0ca
Usman Ali <mrusmanali95@gmail.com>
2023-04-07 06:35:33 +0400

    fixed responsive

--- modules/Articles/index.js
+++ modules/Articles/index.js
@@ -12,14 +12,14 @@ function BlogHome() {
   return (
     <Layout>
       <div className="us_row us_small_p20">
-        <div className="us_columns us_small_17">
+        <div className="us_columns us_xlarge_17 us_large_16">
           <div className="us_row us_small_p20 us_mt40">
             <div className="us_columns us_small_24 us_mb70"><ArticleMini variant="hero" /></div>
             <div className="us_columns us_small_24 us_mb70"><ArticleMini /></div>
             <div className="us_columns us_small_24 us_mb70"><ArticleMini /></div>
           </div>
         </div>
-        <div className="us_columns us_small_7">
+        <div className="us_columns us_xlarge_7 us_large_8">
           <div className="cs_small_24 us_columns us_mt50 us_mb50"><IntroBlock /></div>
           <div className="cs_small_24 us_columns"><CatLinks /></div>
         </div>
